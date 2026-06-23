6e169483cefedd899931ed7cfb2d1ded3d5bdec3
Lexi714 <alexiamarielsaucedo@gmail.com>
2026-06-23 20:18:11 -0300

    perf[UNITASK-97-5-2-1] (resources/js/app.js resources/views/dashboard.blade.php database/migrations/2026_06_23_220020_add_composite_indexes_to_academic_tables.php database/migrations/2026_06_23_223636_add_tasks_indexes_to_tasks_table.php) Se centraliza CSRF AJAX y se agrega indices de tasks

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,23 @@
 import Alpine from 'alpinejs';
 import Sortable from 'sortablejs';
 import Swal from 'sweetalert2';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list';
 
 window.Alpine = Alpine;
 window.Sortable = Sortable;
 window.Swal = Swal;
+
+window.FullCalendar = {
+    Calendar,
+    dayGridPlugin,
+    timeGridPlugin,
+    interactionPlugin,
+    listPlugin,
+};
 
 const csrfToken = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
 
